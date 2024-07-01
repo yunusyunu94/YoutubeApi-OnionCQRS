1c30afc158396e8fa6fdf3d9881de9901e6ae0d5
k-yunus <112016542+k-yunus@users.noreply.github.com>
2024-07-01 21:54:44 +0300

    Temel Entityler oluşturuldu

---
+++ Core/YoutubeApi.Domein/Entities/Brand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domein.Commen;
+
+namespace YoutubeApi.Domein.Entities
+{
+	public class Brand : EntityBase, IEntityBase // zaten EntityBase den miras alıyor yazmasakta olur
+	{
+		public Brand()
+		{
+		}
+
+		public Brand(string name)
+		{
+			Name = name;
+		}
+
+		public required string Name { get; set; }
+
+		public ICollection<Product> Products { get; set; }
+    }
+}
