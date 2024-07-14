732a0e61954146095887c63f6847a8aac0bf5067
k-yunus <112016542+k-yunus@users.noreply.github.com>
2024-07-14 13:31:11 +0300

    WriteRepository yapıldı ve Registrationda Dependency Injectiona eklendi

--- Core/YoutubeApi.Application/İnterfaces/Repositories/IWriteRepository.cs
+++ Core/YoutubeApi.Application/İnterfaces/Repositories/IWriteRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YoutubeApi.Domein.Commen;
 
 namespace YoutubeApi.Application.İnterfaces.Repositories
 {
-	public interface IWriteRepository
+	public interface IWriteRepository<T> where T : class, IEntityBase, new()
 	{
+		Task AddAsync(T entity);
+		Task AddRangeAsync(IList<T> entities);
+		Task<T> UpdateAsync(T entity);
+
+		Task HardDeleteAsync(T entity);
 	}
 }
